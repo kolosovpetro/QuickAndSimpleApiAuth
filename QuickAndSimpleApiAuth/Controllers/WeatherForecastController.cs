d923d032db460f67e957eb7b9aaf1439fb97ce39
kolosovpetro <kolosovp94@gmail.com>
2022-09-17 15:55:55 +0200

    controller roles updated

--- QuickAndSimpleApiAuth/Controllers/WeatherForecastController.cs
+++ QuickAndSimpleApiAuth/Controllers/WeatherForecastController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
 using Microsoft.Identity.Web.Resource;
 
 namespace QuickAndSimpleApiAuth.Controllers;
@@ -13,41 +12,36 @@ namespace QuickAndSimpleApiAuth.Controllers;
 [RequiredScope(RequiredScopesConfigurationKey = "AzureAd:Scopes")]
 public class WeatherForecastController : ControllerBase
 {
-    private static readonly string[] Summaries = new[]
+    private static readonly string[] Summaries =
     {
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
-    private readonly ILogger<WeatherForecastController> _logger;
-
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
-    {
-        _logger = logger;
-    }
-
     [Authorize(Roles = "Manager")]
     [HttpGet("GetWeatherManager")]
-    public IEnumerable<WeatherForecast> GetWeatherManager()
-    {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
-    }
+    public IEnumerable<WeatherForecast> GetWeatherManager() => GetRandomForecast();
+
+    [Authorize(Roles = "Manager, Admin")]
+    [HttpGet("GetWeatherManagerAndAdmin")]
+    public IEnumerable<WeatherForecast> GetWeatherManagerAndAdmin() => GetRandomForecast();
 
     [Authorize]
-    [HttpGet("GetWeather")]
-    public IEnumerable<WeatherForecast> GetWeather()
+    [HttpGet("GetWeatherAll")]
+    public IEnumerable<WeatherForecast> GetWeatherAll() => GetRandomForecast();
+
+    [Authorize(Roles = "Reader")]
+    [HttpGet("GetWeatherReader")]
+    public IEnumerable<WeatherForecast> GetWeatherReader() => GetRandomForecast();
+
+    private static IEnumerable<WeatherForecast> GetRandomForecast()
     {
-        return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-            })
-            .ToArray();
+        var result = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        {
+            Date = DateTime.Now.AddDays(index),
+            TemperatureC = Random.Shared.Next(-20, 55),
+            Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        }).ToArray();
+
+        return result;
     }
 }
